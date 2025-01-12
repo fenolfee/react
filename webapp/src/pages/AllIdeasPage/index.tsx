90f2b41cbda39c0d008bbb28feacff999af65257
fenolfee <fenolfee@hotmail.com>
2025-01-12 19:11:21 +0700

    trpc added to the project

---
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -0,0 +1,18 @@
+import { trpc } from '../../lib/trpc'
+
+export const AllIdeasPage = () => {
+   const {data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
+   if (isLoading || isFetching) return <p>Loading...</p>
+   if (isError) return <p>Error: {error.message}</p>
+   return (
+         <div>
+             <h1>AllIdeas</h1>
+             {data.ideas.map((idea) => (
+                 <div key={idea.nick}>
+                     <h2>{idea.name}</h2>
+                     <p>{idea.description}</p>
+                 </div>
+             ))}
+         </div>
+       )
+ }
